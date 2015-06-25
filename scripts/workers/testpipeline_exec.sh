da63cfe067e69d1f756abe2c40569b43d84eee3a
Bill Wilder <whwilder@email.arizona.edu>
2015-06-25 12:26:04 -0700

    Added some comments; updated command for taxonomy assignment in testpipeline_exec.sh

--- scripts/workers/testpipeline_exec.sh
+++ scripts/workers/testpipeline_exec.sh
@@ -9,6 +9,8 @@
 set -u
 source ${PROJECT_DIR}/config.sh
 
+### STEP 0: FILTER READS ###
+
 cd $FOR_OUT
 while read FILE; do 
    echo $FILE
@@ -50,24 +52,25 @@ cat $TRUNCFILES > ${REV_READS}
 rm *_rev_*.fastq.a[a-z]
 
 
+### STEP 1: FILTER BARCODES TO MATCH FILTERED READS ###
+
 cd $FOR_OUT
 FOR_BASENAME=$(basename $FOR_READS .fastq)
 ### GENERATE LIST OF SEQUENCES IN FILTERED FILE
 grep '@M' ${FOR_READS} | sed 's/@//' > ${BASE}.accnos
 ### GET SEQUENCES FROM BARCODE READS TO MATCH FILTERED READS
-#mothur "#get.seqs(accnos=$FOR_BASENAME.accnos2, fastq=${DATA_DIR}/run1_bc.fastq)"
 ${BIN_DIR}/usearch -fastx_getseqs ${BARCODES} -labels ${BASE}.accnos -fastqout ${FOR_BARCODES} 2> 01-bc-filter.log
 ### REMOVE ENDING ON BARCODE FILE NAME TO MATCH FILTERED READS FILE
-#sed 's/ 1:N:0:0//' ${DATA_DIR}/run1_bc.pick.fastq > run1_bc.pick2.fastq
 
 ### Do above steps for the reverse reads
 ### note to self - find a way to do forward and reverse reads in one step
 cd $REV_OUT
 REV_BASENAME=$(basename $REV_READS .fastq)
 grep '@M' ${REV_READS} | sed 's/@//' > ${BASE}.accnos
-#mothur "#get.seqs(accnos=$REV_BASENAME.accnos2, fastq=${DATA_DIR}/run1_bc.fastq)"
 ${BIN_DIR}/usearch -fastx_getseqs ${BARCODES} -labels ${BASE}.accnos -fastqout ${REV_BARCODES} 2> 01-bc-filter.log
-#sed 's/1:N:0:0//' ${REV_BASENAME}.pick.fastq > ${REV_BASENAME}.pick2.fastq
+
+
+### STEP 2: DEMULTIPLEX ###
 
 module load qiime/1.8.0
 
@@ -82,6 +85,8 @@ split_libraries_fastq.py -o . -i ${REV_READS} -b ${REV_BARCODES} -m ${MAPFILE} -
 mv seqs.fna ${REV_DEMUX}
 
 
+### STEP 3: DEREPLICATE ###
+
 cd $FOR_OUT
 ${BIN_DIR}/usearch -derep_fulllength ${FOR_DEMUX} -fastaout ${FOR_DEREP} -sizeout -threads 1 &> 03-usearch-derep.log
 ### Do above steps for the reverse reads
@@ -89,6 +94,8 @@ ${BIN_DIR}/usearch -derep_fulllength ${FOR_DEMUX} -fastaout ${FOR_DEREP} -sizeou
 cd $REV_OUT
 ${BIN_DIR}/usearch -derep_fulllength ${REV_DEMUX} -fastaout ${REV_DEREP} -sizeout -threads 1 &> 03-usearch-derep.log
 
+
+### STEP 4: SORT BY SIZE, REMOVE SINGLETONS ###
 
 cd $FOR_OUT
 ${BIN_DIR}/usearch -sortbysize ${FOR_DEREP} -fastaout "${FOR_SORTED}" -minsize ${MIN_SIZE} 2> 04-sort-discard.log
@@ -99,6 +106,8 @@ REV_BASENAME=$(basename ${REV_DEREP} .fna)
 ${BIN_DIR}/usearch -sortbysize ${REV_DEREP} -fastaout ${REV_SORTED} -minsize ${MIN_SIZE} 2> 04-sort-discard.log
 
 
+### STEP 5: CLUSTER INTO OTUS ###
+
 cd $FOR_OUT
 ${BIN_DIR}/usearch -cluster_otus ${FOR_SORTED} -otus ${FOR_OTU} -uparseout ${FOR_OTU_RESULTS} -otu_radius_pct ${OTU_RADIUS} 2> 05-for_otu_cluster_5.0.log
 ### Do above steps for the reverse reads
@@ -107,6 +116,8 @@ cd $REV_OUT
 REV_BASENAME=$(basename ${REV_SORTED} _derep_sorted.fna)
 ${BIN_DIR}/usearch -cluster_otus ${REV_SORTED} -otus ${REV_OTU} -uparseout ${REV_OTU_RESULTS} -otu_radius_pct ${OTU_RADIUS} 2> 05-rev_otu_cluster_5.0.log
 
+
+### STEP 6: CHECK FOR CHIMERAS ###
 
 cd $FOR_OUT
 FOR_OTU_BASE=$(basename ${FOR_OTU#*05-*} .fasta)
@@ -118,6 +129,8 @@ REV_OTU_BASE=$(basename ${REV_OTU#*05-*} .fasta)
 ${BIN_DIR}/usearch -uchime_ref ${REV_OTU} -db ${ITS_REF_DATABASE} -uchimeout 06-chimeras.uchime -strand plus -nonchimeras ${REV_NONCHIM} -threads 1 2> 06-chimera-check.log
 
 
+### STEP 7: MAP DEMULTIPLEXED READS BACK TO CHIMERA-CHECKED OTUS ###
+
 cd $FOR_OUT
 ${BIN_DIR}/usearch -usearch_global ${FOR_DEMUX} -db ${FOR_NONCHIM} -strand plus -id 0.97 -uc ${FOR_CHIM_MAP} -threads 1 2> 07-map.for_ee${MAXEE}_trunc${TRUNCVAL}_uchime.log
 ### Do above steps for the reverse reads
@@ -125,6 +138,8 @@ ${BIN_DIR}/usearch -usearch_global ${FOR_DEMUX} -db ${FOR_NONCHIM} -strand plus 
 cd $REV_OUT
 ${BIN_DIR}/usearch -usearch_global ${REV_DEMUX} -db ${REV_NONCHIM} -strand plus -id 0.97 -uc ${REV_CHIM_MAP} -threads 1 2> 07-map.rev_ee${MAXEE}_trunc${TRUNCVAL}_uchime.log
 
+
+### STEP 8: BUILD OTU TABLE ###
 
 cd $FOR_OUT
 python ${BIN_DIR}/drive5_py/uc2otutab_mod.py ${FOR_CHIM_MAP} > \
@@ -136,17 +151,18 @@ python ${BIN_DIR}/drive5_py/uc2otutab_mod.py ${FOR_CHIM_MAP} > \
 ${REV_OTU_TABLE} 2> 08-otu-table_rev.log
 
 
+### STEP 9: ASSIGN TAXONOMY ###
+
 module load qiime/1.9.0
 module load mothur
 
 cd $FOR_OUT
-#parallel_assign_taxonomy_rdp.py -i ${FOR_NONCHIM} -o ${FOR_TAXONOMY} -t ${QIIME_TAX_MAP} -r ${QIIME_REF_DATABASE} -O 7 --rdp_max_memory 3500
-mothur "#classify.seqs(fasta=${FOR_NONCHIM}, reference=${MOTHUR_REF_DATABASE}, taxonomy=${MOTHUR_TAX_MAP}, cutoff=80, processors=8)" 2> 09_assign_taxonomy.log
-mv ${FOR_NONCHIM%.uchime}.UNITEv6_sh_97.wang.taxonomy ${FOR_TAXONOMY}
+parallel_assign_taxonomy_rdp.py -i ${FOR_NONCHIM} -o . -t ${QIIME_TAX_MAP} -r ${QIIME_REF_DATABASE} -O 4 --rdp_max_memory 3500 2> 09_assign_taxonomy.log
+# rename output to something less confusing
+mv ${FOR_NONCHIM%.uchime}_tax_assignments.txt ${FOR_TAXONOMY}
 
 ### Do above steps for the reverse reads
 ### note to self - find a way to do forward and reverse reads in one step
 cd $REV_OUT
-mothur "#classify.seqs(fasta=${REV_NONCHIM}, reference=${MOTHUR_REF_DATABASE}, taxonomy=${MOTHUR_TAX_MAP}, cutoff=80, processors=8)" 2> 09_assign_taxonomy.log
-mv ${REV_NONCHIM%.uchime}.UNITEv6_sh_97.wang.taxonomy ${REV_TAXONOMY}
-#parallel_assign_taxonomy_rdp.py -i ${REV_NONCHIM} -o ${REV_TAXONOMY} -t ${TAX_MAP} -r ${REF_DATABASE} -O 7 --rdp_max_memory 3500
+parallel_assign_taxonomy_rdp.py -i ${REV_NONCHIM} -o . -t ${QIIME_TAX_MAP} -r ${REF_DATABASE} -O 4 --rdp_max_memory 3500 2> 09_assign_taxonomy.log
+mv ${REV_NONCHIM%.uchime}_tax_assignments.txt ${REV_TAXONOMY}
